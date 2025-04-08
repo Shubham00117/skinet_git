f7f07f0f283ae609d831e0e0e592cdea89c9f93a
Shubham00117 <shubhamshinde5150@gmail.com>
2025-04-08 19:22:12 +0530

    stripe_updated_pubilc_Key

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://localhost:5001/api/',
   hubUrl: 'https://localhost:5001/hub/notifications',
   stripePublicKey:
-    'pk_test_51R8HvURKPJZ5dsPEhozmUyjDV5sfrIGf8djk9xte4K35rqrgyK0Ehv7iEAXxzDjzBEAtEkldV7Bfcitt2X6eIpbT00lXUd0xpV',
+    'pk_test_51RAQI6BceHsZGzIMdLNT217jmUJg4svrWPwoQGuTybtgvZI5hb3wpUymXcvcNo7b7UwOZYOUfCMS3XBVXZOntPJp003ADIn7Cr',
 };
