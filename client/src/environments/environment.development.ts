7c771607fdbdf9acf0aba2023dc59cec4514e134
Shubham00117 <shubhamshinde5150@gmail.com>
2025-04-08 18:59:15 +0530

    Pre Continuous integration

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
+  stripePublicKey:
+    'pk_test_51R8HvURKPJZ5dsPEhozmUyjDV5sfrIGf8djk9xte4K35rqrgyK0Ehv7iEAXxzDjzBEAtEkldV7Bfcitt2X6eIpbT00lXUd0xpV',
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'api/',
+  hubUrl: 'hub/notifications',
+  stripePublicKey:
+    'pk_test_51RAQI6BceHsZGzIMdLNT217jmUJg4svrWPwoQGuTybtgvZI5hb3wpUymXcvcNo7b7UwOZYOUfCMS3XBVXZOntPJp003ADIn7Cr',
+};
